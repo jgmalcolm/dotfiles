2052e2cce076e60e39d86340019f01df6182afee
malcolm <malcolm@accelereyes.com>
2012-08-21 18:18:43 -0400

    ensure ssh/config has proper perms

--- update.sh
+++ update.sh
@@ -9,7 +9,8 @@ link() { rm -rf $HOME/.$1; ln -s $dir/$1 $HOME/.$1; }
 copy() { rm -rf $HOME/.$1; cp -p $dir/$1 $HOME/.$1; }
 
 # ssh,gdb hate symlinks
-cp -p $dir/ssh_config   $HOME/.ssh/config
+cp $dir/ssh_config   $HOME/.ssh/config
+chmod 644 $HOME/.ssh/config
 copy gdbinit
 
 # these guys are fine with symlinks
